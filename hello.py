b7c98739b201c8aedbcb8088cd6782c9c354bdd0
vkravchuk <vkravchuk@desyde.com.ua>
2022-01-01 23:26:21 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -94,3 +94,50 @@ for musician in musicians:
 	print(musician)
 	print('------')
 print('They are great musicians')
+
+for value in range(1, 5):
+	print(value)
+
+numbers = list(range(1, 6))
+print(numbers)
+
+even_numbers = list(range(2, 11, 2)) #third prams is step
+print(even_numbers)
+
+squares = []
+for value in range(1, 11):
+	square = value ** 2 #value^2
+	squares.append(square)
+
+print(squares)
+
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+
+squares = [value**2 for value in range(1, 11)]
+print(squares)
+
+players = ['charles', 'martina', 'michael', 'mark', 'florende', 'eli']
+print(players)
+#slices:
+print(players[0:3]) 
+print(players[1:4])
+print(players[:4])
+print(players[2:])
+print(players[-3:])
+
+for player in players[:3]:
+	print(player.title())
+
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:] #create copy of array
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print(my_foods)
+print(friend_foods)
+
